3a7d8ce9e6d797282a61c42ca3d3ad18665fd44d
Jonathan Reem <jonathan.reem@gmail.com>
2013-07-19 00:50:44 +0000

    debugging web.js:

--- web.js
+++ web.js
@@ -7,11 +7,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.write(index);
+app.get('/index.html', function(req, res) {
+   res.send(index)
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
