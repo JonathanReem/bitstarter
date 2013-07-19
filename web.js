6b3136c6d73fd6229e6cf46beb6156925a8203b1
Jonathan Reem <jonathan.reem@gmail.com>
2013-07-19 00:57:17 +0000

    debugging web.js:

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(req, res) {
-   res.send(index)
+   res.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
