25c37ee329fe5b63849c21a96ff720f00ecd1a53
Jonathan Reem <jonathan.reem@gmail.com>
2013-07-19 00:32:05 +0000

    changed web.js

---
+++ web.js
@@ -0,0 +1,19 @@
+
+var http = require('http');
+var fs = require('fs');
+var buffer = require('buffer');
+var index = fs.readFile('./index.html');
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var body = index;
+  response.write(body);
+  response.end();
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
